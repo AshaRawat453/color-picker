c6e8baa0711bb901c70595cb7769a893f72548f2
AshaRawat453 <ashurwt455@gmail.com>
2024-06-24 17:17:13 +0530

    Add files via upload

---
+++ 5Atask1.js
@@ -0,0 +1,23 @@
+function changecolor1(){
+        document.getElementsByClassName("container")[0].style.backgroundColor = "#e0e0e0";
+        document.querySelector("span").textContent="#e0e0e0";
+        document.querySelector("span").style.color="#e0e0e0";
+}
+function changecolor2(){
+    document.getElementsByClassName("container")[0].style.backgroundColor = "#6fcf97";
+    document.querySelector("span").textContent="#6fcf97";
+    document.querySelector("span").style.color="#6fcf97";
+}
+function changecolor3(){
+    document.getElementsByClassName("container")[0].style.backgroundColor = "#56ccf2";
+    document.querySelector("span").textContent="#56ccf2";
+    document.querySelector("span").style.color="#56ccf2";
+}
+function changecolor4(){
+    document.getElementsByClassName("container")[0].style.backgroundColor = "#bb6bd9";
+    document.querySelector("span").textContent="#bb6bd9";
+    document.querySelector("span").style.color="#bb6bd9";
+}
+
+
+ 
